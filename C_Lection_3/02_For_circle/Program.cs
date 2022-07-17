aa51b9a4228fd9c7c4e5bbfc88a786a653f8f07c
a.fedotov <zacyla@gmail.com>
2022-07-17 19:48:45 +0300

    2 done 3 add

--- C_Lection_3/02_For_circle/Program.cs
+++ C_Lection_3/02_For_circle/Program.cs
@@ -1,2 +1,32 @@
-﻿// Цикл For
+﻿// Цикл For или цикл со счетчиком.
 
+/* for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i)
+}
+*/
+
+// Версия 4ого метода с использованием цикла FOR
+/* string Method4(int count, string text)
+{
+
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+
+
+    }
+    return result;
+}
+*/
+
+// Цикл в цикле. Задача - вывод таблицы умножения на экран.
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}"); // интерполяция строк 
+    }
+    Console.WriteLine();
+}

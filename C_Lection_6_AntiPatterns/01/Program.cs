15d9566b567e1fc0f647f1f09cde43b721b6201e
a.fedotov <zacyla@gmail.com>
2022-08-22 13:24:05 +0300

    1st is rdy

---
+++ C_Lection_6_AntiPatterns/01/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Даны 4 пары чисел в строке и скобках, нужно умножить их на 2.
+*/
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+.Replace("(", "")
+.Replace(")", "");
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+.Select(item => item.Split(','))
+.Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+.ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i].x * 2);
+    Console.WriteLine();
+}
